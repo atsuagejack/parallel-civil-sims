6f2c2912784aaf304fc2da0d4f641bf0b19a5283
atsuagejack <65150335+atsuagejack@users.noreply.github.com>
2026-03-06 04:30:31 +0900

    Update game.js

--- game.js
+++ game.js
@@ -1,57 +1,7 @@
-let stats = {
-order:50,
-freedom:50,
-prosperity:50,
-tech:50
-};
+const SUPABASE_URL = "https://khfhzsrzyzyojacdvrfe.supabase.co"
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtoZmh6c3J6eXp5b2phY2R2cmZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI3MzgxNjYsImV4cCI6MjA4ODMxNDE2Nn0.ZBrIyT2Tv8dyzkBe9xyhnXB7EIW23-bQQRbS8YlzPag"
 
-let events=[];
-
-fetch("events.json")
-.then(res=>res.json())
-.then(data=>{
-events=data;
-nextEvent();
-});
-
-function updateUI(){
-
-document.getElementById("order").textContent=stats.order;
-document.getElementById("freedom").textContent=stats.freedom;
-document.getElementById("prosperity").textContent=stats.prosperity;
-document.getElementById("tech").textContent=stats.tech;
-
-}
-
-function nextEvent(){
-
-let e = events[Math.floor(Math.random()*events.length)];
-
-document.getElementById("event").textContent=e.text;
-
-let choicesDiv=document.getElementById("choices");
-choicesDiv.innerHTML="";
-
-e.choices.forEach(choice=>{
-
-let btn=document.createElement("button");
-btn.textContent=choice.text;
-
-btn.onclick=()=>{
-
-for(let k in choice.effects){
-stats[k]+=choice.effects[k];
-}
-
-updateUI();
-nextEvent();
-
-};
-
-choicesDiv.appendChild(btn);
-
-});
-
-}
-
-updateUI();
+const supabase = supabase.createClient(
+SUPABASE_URL,
+SUPABASE_KEY
+)
